0ba025b90a03c2279346980deb5fa9eb20719798
Ayushi shrimal <141023363+ayushishrigithub@users.noreply.github.com>
2024-05-11 12:27:36 +0530

    Update Grid.js

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -7,7 +7,7 @@ const Grid = ({ photos }) => {
 
     <div className="grid">
         {photos.map(({ photo, _id }) => (
-            <div keya={_id} className="grid_item">
+            <div key={_id} className="grid_item">
                 <img src={'http://localhost:5000/uploads/${photo}'}
                 alt="grid_image"
                 />
